f616f30f5937cf951852cb026b4e5dcc9bb9731d
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2023-03-23 17:45:48 -0400

    vpc

--- README.md
+++
@@ -1,2 +0,0 @@
-# tfc
-heloo
---
+++ main.tf
@@ -0,0 +1,5 @@
+module "vault-starter_example_gcp-vpc" {
+  source  = "hashicorp/vault-starter/gcp//examples/gcp-vpc"
+  version = "0.1.0"
+  # insert the 1 required variable here
+}
