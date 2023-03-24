aad3881f3e35e62af46cfa3af4e2649eb7dfa126
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2023-03-24 14:39:23 -0400

    hh

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "google" {
-    project = "playground-s-11-dd33d0a9"
+    project = "playground-s-11-763b2634"
     region = "us-central1-a"
 }
 
 resource "google_storage_bucket" "static-site" {
   name          = "naztest87890655366337737"
-  location      = "EU"
+  location      = "US-CENTRAL1"
 }
