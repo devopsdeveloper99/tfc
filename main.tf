d76e6b39fc921d8d2d9b9231066f42cd0cf0d1e5
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2023-03-24 11:25:09 -0400

    hh

--- main.tf
+++ main.tf
@@ -1,21 +1,4 @@
-provider "google" {
-  project = "playground-s-11-dd33d0a9"
-}
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "EU"
-  force_destroy = true
-
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
+resource "google_project" "my_project" {
+  name       = "My Project99"
+  project_id = "1234567777777"
 }
