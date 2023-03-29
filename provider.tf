efbfa66d7a019a3f92daa018ecd930814f75f30c
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2023-03-29 15:14:21 -0400

    kjfhkjjj

--- provider.tf
+++ provider.tf
@@ -1,14 +1,30 @@
+# terraform {
+#   required_providers {
+#     google = {
+#       source = "hashicorp/google"
+#       version = "4.59.0"
+#     }
+#   }
+# }
+
+
+# provider "google" {
+#   project = "avian-amulet-378416"
+#   region  = "us-central1"
+#   zone    = "us-central1-c"
+# }
+
+
 terraform {
   required_providers {
-    google = {
-      source = "hashicorp/google"
+    google-beta = {
+      source = "hashicorp/google-beta"
       version = "4.59.0"
     }
   }
 }
 
-
-provider "google" {
+provider "google-beta" {
   project = "avian-amulet-378416"
   region  = "us-central1"
   zone    = "us-central1-c"
