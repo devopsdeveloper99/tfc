ecdcb78d1ed328bf46ed9831861f40492d17a708
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2023-03-30 11:21:27 -0400

    hjhg

--- firestore.tf
+++ firestore.tf
@@ -1,29 +1,29 @@
-resource "google_storage_bucket" "avian-amulet-378416" {
-  provider                    = google-beta
-  project   = "avian-amulet-378416"
-  name                        = "test_bucket696969"
-  location                    = "northamerica-northeast1"
-  uniform_bucket_level_access = true
-}
-
-resource "google_firebase_storage_bucket" "avian-amulet-378416" {
-  provider  = google-beta
-  project   = "avian-amulet-378416"
-  bucket_id = google_storage_bucket.avian-amulet-378416.id
-}
-
-
-
-# resource "google_storage_bucket" "default" {
+# resource "google_storage_bucket" "avian-amulet-378416" {
 #   provider                    = google-beta
-#   name                        = "cloud_storage_firebase_bucket_1_nazmul"
-#   location                    = "northamerica-northeast1"
 #   project   = "avian-amulet-378416"
+#   name                        = "test_bucket696969"
+#   location                    = "northamerica-northeast1"
 #   uniform_bucket_level_access = true
 # }
 
-# resource "google_firebase_storage_bucket" "default" {
+# resource "google_firebase_storage_bucket" "avian-amulet-378416" {
 #   provider  = google-beta
 #   project   = "avian-amulet-378416"
-#   bucket_id = google_storage_bucket.default.id
+#   bucket_id = google_storage_bucket.avian-amulet-378416.id
 # }
+
+
+
+resource "google_storage_bucket" "default" {
+  provider                    = google-beta
+  name                        = "cloud_storage_firebase_bucket_1_nazmul"
+  location                    = "northamerica-northeast1"
+  project   = "avian-amulet-378416"
+  uniform_bucket_level_access = true
+}
+
+resource "google_firebase_storage_bucket" "default" {
+  provider  = google-beta
+  project   = "avian-amulet-378416"
+  bucket_id = google_storage_bucket.default.id
+}
