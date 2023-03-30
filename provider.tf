8982243ea01681eee7a0012767431e8dd0a06bfe
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2023-03-30 11:25:25 -0400

    jhgjshdf

--- provider.tf
+++ provider.tf
@@ -15,17 +15,34 @@
 # }
 
 
+# terraform {
+#   required_providers {
+#     google-beta = {
+#       source = "hashicorp/google-beta"
+#       version = "4.59.0"
+#     }
+#   }
+# }
+
+# provider "google-beta" {
+#   project = "avian-amulet-378416"
+#   region  = "us-central1"
+#   zone    = "us-central1-c"
+# }
+
+# Terraform configuration to set up providers by version.
 terraform {
   required_providers {
     google-beta = {
-      source = "hashicorp/google-beta"
-      version = "4.59.0"
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
     }
   }
 }
 
+# Configure the provider to use a specific project, by default.
+# In this guide, this project is referred to as the "quota-check project".
+# Including `user_project_override = true` is required if a `billing_project` is specified.
 provider "google-beta" {
-  project = "avian-amulet-378416"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  user_project_override = true
 }
